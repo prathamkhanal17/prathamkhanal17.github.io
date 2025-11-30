7372a7bfaa5f58654f828497cd5a2c522edc663d
Pratham Khanal <prathamkhanal962@gmail.com>
2025-11-30 22:29:21 +0545

    chore: Remove console greeting log statement

--- script.js
+++ script.js
@@ -4,10 +4,4 @@ document.addEventListener('DOMContentLoaded', () => {
     if (yearSpan) {
         yearSpan.textContent = new Date().getFullYear();
     }
-
-    // Console greeting
-    console.log(
-        '%c Hello. ',
-        'background: #111; color: #fff; padding: 5px; border-radius: 3px;'
-    );
 });
